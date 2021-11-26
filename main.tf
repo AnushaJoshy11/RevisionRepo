7bbf76eb968dc74e643ca14de035e26274abb77e
AnushaJoshy11 <anusha.joshell@gmail.com>
2021-11-26 13:05:50 +0000

    added sse s3

--- main.tf
+++ main.tf
@@ -48,21 +48,21 @@ provider "aws" {
       enabled = true
     }
     #server_side_encryption_configuration = var.server_side_encryption_configuration
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-          kms_master_key_id = aws_kms_key.mykey.arn
+    server_side_encryption_configuration = {
+      rule = {
+        apply_server_side_encryption_by_default = {
+         # kms_master_key_id = aws_kms_key.mykey.arn
           sse_algorithm     = "aws:kms"
         }
       }
-    }    
+    }   
   }
 
 
-  resource "aws_kms_key" "mykey" {
-    description             = "This key is used to encrypt bucket objects"
-    deletion_window_in_days = 10
-  }
+  # resource "aws_kms_key" "mykey" {
+  #   description             = "This key is used to encrypt bucket objects"
+  #   deletion_window_in_days = 10
+  # }
 
 # resource "aws_s3_bucket" "mybucket" {
 #   bucket = "mybucket"
